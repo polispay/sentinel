71f4f0d45eaebd5b31249d5ac67135b97ae0c603
POLIS <34659464+polispay@users.noreply.github.com>
2017-12-22 07:25:26 -0600

    Update test_jsonrpc.py

--- test/integration/test_jsonrpc.py
+++ test/integration/test_jsonrpc.py
@@ -16,12 +16,12 @@ def test_polisd():
     config_text = PolisConfig.slurp_config_file(config.polis_conf)
     network = 'mainnet'
     is_testnet = False
-    genesis_hash = u'00000ffd590b1485b3caadc19b22e6379c733355108f107a430458cdf3407ab6'
+    genesis_hash = u'000009701eb781a8113b1af1d814e2f060f6408a2c990db291bc5108a1345c1e'
     for line in config_text.split("\n"):
         if line.startswith('testnet=1'):
             network = 'testnet'
             is_testnet = True
-            genesis_hash = u'00000bafbc94add76cb75e2ec92894837288a481e5c005f6563d91623bf8bc2c'
+            genesis_hash = u'000009701eb781a8113b1af1d814e2f060f6408a2c990db291bc5108a1345c1e'
 
     creds = PolisConfig.get_rpc_creds(config_text, network)
     polisd = PolisDaemon(**creds)
