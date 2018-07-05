341f19ce548cd53fa996fbb0928d840e2b68e470
cronos <cronos@polispay.org>
2018-07-05 13:31:23 -0500

    More address replace

--- test/unit/models/test_proposals.py
+++ test/unit/models/test_proposals.py
@@ -162,7 +162,7 @@ def test_proposal_is_valid(proposal):
     proposal.payment_address = '7'
     assert proposal.is_valid() is False
 
-    proposal.payment_address = 'YYE8KWYAUU5YSWSYMB3Q3RYX8XTUU9Y7UI'
+    proposal.payment_address = 'TXDSaTXerg68SCyLkWw2ERsqoTMWRBZiZu'
     assert proposal.is_valid() is False
 
     proposal.payment_address = 'TXDSaTXerg68SCyLkWw2ERsqoTMWRBZiZr'
